d730526b405b094f72d88779d5cd216f5f503aaf
wesleyserafim <wesley@eguana.com.br>
2021-11-17 18:02:38 -0300

    Alterando estrutura de models para separar os relacionamentos entre as models.

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -12,12 +12,35 @@ import (
 	"github.com/wesleysaraujo/go-graphql/graph/model"
 )
 
+func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
+	var courses []*model.Course
+
+	for _, v := range r.Resolver.Courses {
+		if v.Category.ID == obj.ID {
+			courses = append(courses, v)
+		}
+	}
+
+	return courses, nil
+}
+
+func (r *courseResolver) Chapters(ctx context.Context, obj *model.Course) ([]*model.Chapter, error) {
+	var chapters []*model.Chapter
+
+	for _, v := range r.Resolver.Chapters {
+		if v.Course.ID == obj.ID {
+			chapters = append(chapters, v)
+		}
+	}
+
+	return chapters, nil
+}
+
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
 	category := model.Category{
 		ID:          fmt.Sprintf("T%d", rand.Int()),
 		Name:        input.Name,
 		Description: &input.Description,
-		Courses:     []*model.Course{},
 	}
 
 	r.Categories = append(r.Categories, &category)
@@ -78,11 +101,19 @@ func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) 
 	return r.Resolver.Chapters, nil
 }
 
+// Category returns generated.CategoryResolver implementation.
+func (r *Resolver) Category() generated.CategoryResolver { return &categoryResolver{r} }
+
+// Course returns generated.CourseResolver implementation.
+func (r *Resolver) Course() generated.CourseResolver { return &courseResolver{r} }
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
--- readme.md
+++ readme.md
@@ -1,1 +1,90 @@
 ## Go Lang GraphQL
+
+Exemple de API GraphQL para cadastro e consulta de:
+
+* Categories
+* Courses
+* Chapters
+
+Esse exemplo utiliza o módulo (gqlgen)[https://gqlgen.com/] para gerar o scaffold com seus devidos modelos
+
+### Implementação dos principais métodos
+Os principais métodos responsáveis pelas consultas e mutations estão implementados em:
+`graph/schema.resolvers.go`
+
+### Rodar o servidor GraphQL (Playground)
+`go run ./server.go`
+
+### Mutations
+`mutation createCategory {
+  createCategory(input: {
+    name: "Linux",
+    description: "Cursos de Linux"
+  }) {
+    id
+    name
+    description
+  }
+}
+
+mutation createCourse {
+  createCourse(input: {
+    name: "Linux para Desenvolvedores",
+    description: "Cursos de Ubuntu Linux para desenvolvedores"
+    categoryId: "T5577006791947779410"
+  }) {
+    id
+    name
+    description
+  }
+}
+
+mutation createChapter {
+  createChapter(input: {
+    name: "Primeiros Passos",
+    courseId: "T8674665223082153551"
+  }) {
+    id
+    name
+  }
+}`
+
+### Query
+`query findCategories {
+  categories {
+    id
+    name
+    description
+    courses {
+      name
+    }
+  }
+}
+
+query findCourses {
+  courses {
+    id
+    name
+    description
+    category {
+      id
+      name
+    },
+    chapters {
+      id
+      namehttps://gqlgen.com/
+    }
+  }
+}
+
+query findChapters {
+  chapters {
+    id
+    name
+    course {
+      name
+    }
+  }
+}
+`
+
